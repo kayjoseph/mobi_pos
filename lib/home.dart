7960a5075fdc72064584c5834e364e2c564989c7
Joseph Kay <kayjoseph049@gmail.com>
2026-05-25 16:59:23 +0300

    update on HomePage

--- lib/home.dart
+++ lib/home.dart
@@ -5,6 +5,11 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('HomePage'),
+      ),
+    );
   }
 }
