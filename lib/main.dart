43b85090fe5c699bbe0887138d87552eb73b6351
Joseph Kay <kayjoseph049@gmail.com>
2026-05-26 12:39:43 +0300

    update on main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobi_pos/home.dart';
+import 'package:mobi_pos/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: LoginPage(),
     );
   }
 }
