2155f80ecf8ec11333f8c7ea519fa0efe4f706e7
Joseph Kay <kayjoseph049@gmail.com>
2026-06-09 10:19:16 +0300

    update on splash

---
+++ lib/splash_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:mobi_pos/login_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    // Wait 5 seconds then navigate to login
+    Future.delayed(const Duration(seconds: 5), () {
+      if (mounted) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const LoginPage()),
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset(
+          'images/mobipos_logo2.png',
+          width: 250,
+          height: 250,
+        ),
+      ),
+    );
+  }
+}
