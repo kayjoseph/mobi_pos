b29ae4445e48b2943d46ab23085d6d07c4ab37d0
Joseph Kay <kayjoseph049@gmail.com>
2026-06-16 11:34:03 +0300

    update on purchase

---
+++ lib/purchase.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Purchase extends StatefulWidget {
+  const Purchase({super.key});
+
+  @override
+  State<Purchase> createState() => _PurchaseState();
+}
+
+class _PurchaseState extends State<Purchase> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
