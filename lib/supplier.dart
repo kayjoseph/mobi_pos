ce314122174719d4eaa32297f26b61306100329b
Joseph Kay <kayjoseph049@gmail.com>
2026-06-25 11:25:17 +0300

    update on supplier module

---
+++ lib/supplier.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Supplier extends StatefulWidget {
+  const Supplier({super.key});
+
+  @override
+  State<Supplier> createState() => _SupplierState();
+}
+
+class _SupplierState extends State<Supplier> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+      ),
+    );
+  }
+}
