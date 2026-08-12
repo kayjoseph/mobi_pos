612304f9856c2cc4e8be3ed0c1ed8b94a1fab354
Joseph Kay <kayjoseph049@gmail.com>
2026-08-12 09:32:16 +0300

    update on home

--- lib/home.dart
+++ lib/home.dart
@@ -3,3 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:mobi_pos/login_page.dart';
 import 'package:mobi_pos/app_drawer.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+
+class Home extends StatefulWidget {
+  final String username;
+
+  const Home({super.key, required this.username});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final GlobalKey<ScaffoldState> _scaffoldKey =
+  GlobalKey<ScaffoldState>();
+  final supabase = Supabase.instance.client;
